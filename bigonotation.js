4cfa6b62c844020eca16c04a84c43cdf19c1428c
santhoshkumar <its.santhoshkumar@outlook.com>
2023-06-17 18:34:54 +0530

    fix: increased optmizied

--- bigonotation.js
+++ bigonotation.js
@@ -1,9 +1,6 @@
 const lists = ['a', 'b', 'c'];
-const lists1 = [1, 2, 3, 4, 5, 6];
-for (let j = 0; j < lists1.length; j++) { // runs 6 times
-    for (let i = 0; i < lists.length; i++) { // runs 18  times for each outer loop items it runs the 3 times
-        console.log(lists[i], lists1[j]);
-    }
+for (let i = 0; i < lists.length; i++) { // runs 18  times for each outer loop items it runs the 3 times
+    console.log(lists[i]);
 }
 // --> core part of alogrithm is console.log the each element in the lists array
 // n is the length of dataset length of lists variable
