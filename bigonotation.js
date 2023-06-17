89b899d9287a760c535071469ade1d6903755a52
santhoshkumar <its.santhoshkumar@outlook.com>
2023-06-17 19:29:59 +0530

    n square notation

--- bigonotation.js
+++ bigonotation.js
@@ -1,7 +1,7 @@
-const lists = ['a', 'b', 'c'];
-for (let i = 0; i < lists.length; i++) { // runs 18  times for each outer loop items it runs the 3 times
-    console.log(lists[i]);
-}
+// const lists = ['a', 'b', 'c'];
+// for (let i = 0; i < lists.length; i++) {
+//     console.log(lists[i]);
+// }
 // --> core part of alogrithm is console.log the each element in the lists array
 // n is the length of dataset length of lists variable
 // a is the length of dataset length of lists1 variable
@@ -52,3 +52,32 @@ for (let i = 0; i < lists.length; i++) { // runs 18  times for each outer loop i
     we don't want to care about the slowly growing n because it doesn't affect the performance
     we care about n^2 times
 */
+// O(1) because it have only one constant space doesn't changes as input size changes
+// const spaceComplexity = ['a', 'b', 'c'];
+// for (let i = 0; i < spaceComplexity.length; i++) {
+//     console.log(spaceComplexity[i]);
+// }
+
+// O(n) because it have only one constant space doesn't changes as input size changes
+/*
+    now its changed the because we used new array this will occupy the space but same size of the input array
+*/
+// const spaceComplexity = ['a', 'b', 'c'];
+// const duplicateArray = [];
+// for (let i = 0; i < spaceComplexity.length; i++) {
+//     duplicateArray[i] = spaceComplexity[i];
+// }
+// O(n^2)
+/*
+    one input
+    one output but it will create inner array of each input element so that it takes the
+*/
+const spaceComplexity = ['a', 'b', 'c'];
+const duplicateArray = [];
+for (let i = 0; i < spaceComplexity.length; i++) {
+    duplicateArray[i] = [];
+    for (let j = 0; j < spaceComplexity.length; j++) {
+        duplicateArray[i][j] = spaceComplexity[i];
+    }
+}
+console.log(duplicateArray);
