df6e0e9716bc5cf470a5ff61feadf0bcf6d9ba1a
santhoshkumar <its.santhoshkumar@outlook.com>
2023-06-17 18:04:59 +0530

    fix: changes in time and space complexity

--- bigonotation.js
+++ bigonotation.js
@@ -1,15 +1,20 @@
 const lists = ['a', 'b', 'c'];
-
-for (let i = 0; i < lists.length; i++) {
-    console.log(lists[i]); // --> core part of alogrithm is console.log the element
+const lists1 = [1, 2, 3, 4, 5, 6];
+for (let j = 0; j < lists1.length; j++) { // runs 6 times
+    console.log(lists1[j]);
 }
-
-// n is the length of dataset length
+for (let i = 0; i < lists.length; i++) { // runs 3 times
+    console.log(lists[i]);
+}
+// --> core part of alogrithm is console.log the each element in the lists array
+// n is the length of dataset length of lists variable
+// a is the length of dataset length of lists1 variable
 /*
     if length goes from 3 to 300
     the for loop runs 3 to 300 times
     now this linear scales because is data grows the
     time also grows
-    
-    O(n)
+
+    O(n) for single dataset list
+    o(n + a) for double dataset list
 */
