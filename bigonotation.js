4649abbe9d9eeb4c7683a6c364c2b721d2b1c47a
santhoshkumar <its.santhoshkumar@outlook.com>
2023-06-17 18:10:06 +0530

    calculating time

--- bigonotation.js
+++ bigonotation.js
@@ -1,10 +1,9 @@
 const lists = ['a', 'b', 'c'];
 const lists1 = [1, 2, 3, 4, 5, 6];
 for (let j = 0; j < lists1.length; j++) { // runs 6 times
-    console.log(lists1[j]);
-}
-for (let i = 0; i < lists.length; i++) { // runs 3 times
-    console.log(lists[i]);
+    for (let i = 0; i < lists.length; i++) { // runs 18  times for each outer loop items it runs the 3 times
+        console.log(lists[i], lists1[j]);
+    }
 }
 // --> core part of alogrithm is console.log the each element in the lists array
 // n is the length of dataset length of lists variable
@@ -16,5 +15,5 @@ for (let i = 0; i < lists.length; i++) { // runs 3 times
     time also grows
 
     O(n) for single dataset list
-    o(n + a) for double dataset list
+    o(n * a) for double dataset list
 */
