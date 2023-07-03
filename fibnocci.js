7ceb106d0f017ecf65ea198bfd000b79107fa0be
kaviyarasan <kaviyarasan.t@twilightitsolutions.com>
2023-07-03 17:50:00 +0530

    feat: added fibnocci.js

---
+++ fibnocci.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {*} n number to be takens as to run deep
+ * @returns 
+ */
+function fib(n) {
+    if (n === 0) {
+        return 0
+    }
+    if (n === 1) {
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+
+console.log(fib(4))
